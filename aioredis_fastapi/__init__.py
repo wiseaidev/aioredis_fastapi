96965cbf4583f177c6245e3b92dfadba9c8c486b
wiseaidev <business@wiseai.dev>
2022-08-05 21:22:30 +0300

    modify init file
    Signed-off-by: wiseaidev <business@wiseai.dev>

---
+++ aioredis_fastapi/__init__.py
@@ -0,0 +1,13 @@
+"""
+frozndict is a python package that acts as an alternative to frozenset,
+but for dictionaries.
+"""
+
+
+__author__ = """Mahmoud Harmouch"""
+__email__ = "business@wiseai.dev"
+__version__ = "0.0.1"
+
+from frozndict.frozendict import (
+    frozendict,
+)
