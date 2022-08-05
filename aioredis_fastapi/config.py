74660faf800584bce769f484fb8f9f4a5dcff1da
wiseaidev <business@wiseai.dev>
2022-08-05 21:21:51 +0300

    modify .gitignore & config
    Signed-off-by: wiseaidev <business@wiseai.dev>

--- aioredis_fastapi/config.py
+++ aioredis_fastapi/config.py
@@ -1,31 +1,54 @@
-from datetime import timedelta
-from typing import Callable, Optional
-from uuid import uuid4
-from functools import lru_cache
+import attr
 from attrs import (
     field,
+    frozen,
     validators,
-    frozen
 )
-import attr
-
+from datetime import (
+    timedelta,
+)
+from functools import (
+    lru_cache,
+)
+from typing import (
+    Callable,
+    Optional,
+)
+from uuid import (
+    uuid4,
+)
 
 
 @frozen
 @attr.s(auto_attribs=True, slots=True, init=False, repr=False)
 class Settings:
-    _redis_url: str = field(default="redis://localhost:6379", validator=validators.instance_of(str), converter=str)
-    _session_id_name: str = field(default="ssid", validator=validators.instance_of(str), converter=str)
-    _session_id_generator: Callable[[], str] = field(default=None, validator=validators.instance_of(Callable), converter=Callable)
-    _expire_time: timedelta = field(default=timedelta(hours=6), validator=validators.instance_of(timedelta))
-    _session_id: str = field(default=None, validator=validators.instance_of(str), converter=str)
+    _redis_url: str = field(
+        default="redis://localhost:6379",
+        validator=validators.instance_of(str),
+        converter=str,
+    )
+    _session_id_name: str = field(
+        default="ssid", validator=validators.instance_of(str), converter=str
+    )
+    _session_id_generator: Callable[[], str] = field(
+        default=None, validator=validators.instance_of(Callable), converter=Callable
+    )
+    _expire_time: timedelta = field(
+        default=timedelta(hours=6), validator=validators.instance_of(timedelta)
+    )
+    _session_id: str = field(
+        default=None, validator=validators.instance_of(str), converter=str
+    )
 
     def __attrs_post_init__(self):
         def inner_session_id_generator() -> str:
             return uuid4().hex
+
         if not self._session_id_generator:
-            object.__setattr__(self, '_session_id_generator', inner_session_id_generator)
-        object.__setattr__(self, '_session_id', self._session_id_generator())
+            object.__setattr__(
+                self, "_session_id_generator", inner_session_id_generator
+            )
+        object.__setattr__(self, "_session_id", self._session_id_generator())
 
     @property
     def redis_url(self) -> str:
@@ -93,7 +116,7 @@ class Settings:
         return self._session_id
 
     def generate_session_id(self) -> str:
-        object.__setattr__(self, '_session_id', self._session_id_generator())
+        object.__setattr__(self, "_session_id", self._session_id_generator())
         return self._session_id
 
     def __repr__(self) -> str:
@@ -107,12 +130,16 @@ class Settings:
         ret += f", session_id='{self.session_id}')"
         return ret
 
+
 @lru_cache
-def settings(redis_url: Optional[str] = "redis://localhost:6379",
-        session_id_name: Optional[str] = "ssid",
-        session_id_generator: Optional[Callable[[], str]] = None,
-        expire_time: Optional[timedelta] = timedelta(hours=6)):
+def settings(
+    redis_url: Optional[str] = "redis://localhost:6379",
+    session_id_name: Optional[str] = "ssid",
+    session_id_generator: Optional[Callable[[], str]] = None,
+    expire_time: Optional[timedelta] = timedelta(hours=6),
+):
 
     return Settings(redis_url, session_id_name, session_id_generator, expire_time)
+
 
 __all__ = ["settings"]
