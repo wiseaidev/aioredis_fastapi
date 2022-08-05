2e35d4e844fd378fae81e07333d659e812a15ef1
wiseaidev <business@wiseai.dev>
2022-08-05 21:23:09 +0300

    add methods
    Signed-off-by: wiseaidev <business@wiseai.dev>

---
+++ aioredis_fastapi/methods.py
@@ -0,0 +1,37 @@
+from typing import Any, Generator
+
+from fastapi import Depends, Request, Response
+
+from config import settings
+from session import SessionStorage
+
+
+def get_session_storage() -> Generator:
+    storage = SessionStorage()
+    storage.set_client()
+    yield storage
+
+
+def get_session(
+    request: Request, session_storage: SessionStorage = Depends(get_session_storage)
+):
+    session_id = request.cookies.get(settings().session_id_name, "")
+    return session_storage[session_id]
+
+
+def get_session_id(request: Request):
+    session_id = request.cookies.get(settings().session_id_name, "")
+    return session_id
+
+
+def set_session(
+    response: Response, session: Any, session_storage: SessionStorage
+) -> str:
+    session_id = session_storage.generate_session_id()
+    session_storage[session_id] = session
+    response.set_cookie(settings().session_id_name, session_id, httponly=True)
+    return session_id
+
+
+def del_session(session_id: str, session_storage: SessionStorage):
+    del session_storage[session_id]
